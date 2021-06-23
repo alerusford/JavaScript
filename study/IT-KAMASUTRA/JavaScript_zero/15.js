99a5935a9e53cf591ea9b5d272ccd89249964d5c
alerusford <31511843+alerusford@users.noreply.github.com>
2021-06-23 09:19:31 +0300

    16_start

--- study/IT-KAMASUTRA/JavaScript_zero/15.js
+++ study/IT-KAMASUTRA/JavaScript_zero/15.js
@@ -4,16 +4,36 @@ var buttonMultiply = document.getElementById('buttonMultiply')
 var buttonDivided = document.getElementById('buttonDivided')
 
 function onButtonPlusClick(params) {
-    console.log('onButtonPlusClick')
+    var input1 = document.getElementById('number1')
+    var input2 = document.getElementById('number2')
+    var number1 = Number(input1.value)
+    var number2 = Number(input2.value)
+    var result = number1 + number2
+    console.log(result)
 }
 function onButtonMinusClick(params) {
-    console.log('onButtonMinusClick')
+    var input1 = document.getElementById('number1')
+    var input2 = document.getElementById('number2')
+    var number1 = Number(input1.value)
+    var number2 = Number(input2.value)
+    var result = number1 - number2
+    console.log(result)
 }
 function onButtonMultiplyClick(params) {
-    console.log('onButtonMultiplyClick')
+    var input1 = document.getElementById('number1')
+    var input2 = document.getElementById('number2')
+    var number1 = Number(input1.value)
+    var number2 = Number(input2.value)
+    var result = number1 * number2
+    console.log(result)
 }
 function onButtonDividedClick(params) {
-    console.log('onButtonDividedClick')
+    var input1 = document.getElementById('number1')
+    var input2 = document.getElementById('number2')
+    var number1 = Number(input1.value)
+    var number2 = Number(input2.value)
+    var result = number1 / number2
+    console.log(result)
 }
 
 buttonPlus.addEventListener('click', onButtonPlusClick)
@@ -21,4 +41,4 @@ buttonMinus.addEventListener('click', onButtonMinusClick)
 buttonMultiply.addEventListener('click', onButtonMultiplyClick)
 buttonDivided.addEventListener('click', onButtonDividedClick)
 
-//https://youtu.be/Ga-9DcY1SNk?t=1845
+// https://youtu.be/8DRvw6qSfE4
