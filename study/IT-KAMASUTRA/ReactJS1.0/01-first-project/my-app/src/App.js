083441f092d3921c9349428978b9dbcc63b90a90
alerusford <31511843+alerusford@users.noreply.github.com>
2021-07-21 10:23:45 +0300

    11_start

--- study/IT-KAMASUTRA/ReactJS1.0/01-first-project/my-app/src/App.js
+++ study/IT-KAMASUTRA/ReactJS1.0/01-first-project/my-app/src/App.js
@@ -1,19 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-import Technologies from './Technologies.js';
-import Header from './Header.js';
+import Header from './Header';
+import Technologies from './Technologies'
+import Footer from './footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
+      <Footer />
     </div>
   );
 };
-
-
-
-
 
 export default App;
