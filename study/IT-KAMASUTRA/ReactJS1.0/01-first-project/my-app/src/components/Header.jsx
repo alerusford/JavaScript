d9f86cd1d24450622b449a49129bab02f836d6b9
alerusford <31511843+alerusford@users.noreply.github.com>
2021-07-22 19:18:50 +0300

    13_pause_git-error

---
+++ study/IT-KAMASUTRA/ReactJS1.0/01-first-project/my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img alt="" src="https://alerusford.github.io/img/logotip.webp" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ study/IT-KAMASUTRA/ReactJS1.0/01-first-project/my-app/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+const Content = () => {
+    return (
+        <div className='profile'>
+            <div><img alt="" src="https://vsthemes.org/uploads/posts/2018-12/1581997520_american-landscape_vsthemes_ru-33.jpg" /></div>
+            <div>ava + description</div>
+            <div>posts
+                <div>new post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Content;
